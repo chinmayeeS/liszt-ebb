781a4ab140084bc19d4875f48e0d24e03eba816c
Manolis Papadakis <manopapad@gmail.com>
2017-11-06 11:10:14 -0800

    Hack around Mac's lack of 'readlink -f'

--- liszt-legion.sh
+++ liszt-legion.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -eu
 
-LISZT_PATH="$(cd "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")" && pwd)"
+LISZT_PATH="$(cd "$(dirname "$(perl -MCwd -le 'print Cwd::abs_path(shift)' "${BASH_SOURCE[0]}")")" && pwd)"
 
 TERRA_PATH="$LISZT_PATH"/include/?.t "$LEGION_PATH"/language/regent.py "$@" -fparallelize 1
