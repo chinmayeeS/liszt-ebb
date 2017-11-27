11dd10687bb4e1cff80dc551b94113a573009d90
Manolis Papadakis <manopapad@gmail.com>
2017-11-26 18:11:17 -0800

    Update scripts, host json library in Liszt

--- liszt-legion.sh
+++ liszt-legion.sh
@@ -2,4 +2,10 @@
 
 LISZT_PATH="$(cd "$(dirname "$(perl -MCwd -le 'print Cwd::abs_path(shift)' "${BASH_SOURCE[0]}")")" && pwd)"
 
-TERRA_PATH="$LISZT_PATH"/include/?.t "$LEGION_PATH"/language/regent.py "$@" -fparallelize 1
+gcc -g -O2 -c -o "$LISZT_PATH"/json.o "$LISZT_PATH"/json.c
+ar rcs "$LISZT_PATH"/libjsonparser.a "$LISZT_PATH"/json.o
+
+TERRA_PATH="$LISZT_PATH"/include/?.t \
+    LIBRARY_PATH="$LIBRARY_PATH:$LISZT_PATH" \
+    INCLUDE_PATH="$LISZT_PATH" \
+    "$LEGION_PATH"/language/regent.py "$@" -fparallelize 1 -fflow 0 -fdebug 1
